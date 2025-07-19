a017abb19205288f71ce9b510359ac231939b10a
Alcides Ramos <alcides.ramos@leadtech.com>
2025-07-19 15:49:08 +0200

    refactor: upgrade to PHP 8.4.10

--- src/tests/Unit/Providers/FooTest.php
+++ src/tests/Unit/Providers/FooTest.php
@@ -5,27 +5,15 @@ declare(strict_types=1);
 namespace UnitTests\Providers;
 
 use App\Providers\Foo;
-use DateTime;
 use DateTimeImmutable;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
-use SlopeIt\ClockMock\ClockMock;
 
 #[CoversClass(Foo::class)]
 final class FooTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        ClockMock::freeze(new DateTime('2024-01-01 00:00:00'));
-    }
-
-    protected function tearDown(): void
-    {
-        ClockMock::reset();
-    }
-
     #[Test]
     #[DataProvider('dataProviderForGetDateTime')]
     public function checkGetDateTime(string $format): void
